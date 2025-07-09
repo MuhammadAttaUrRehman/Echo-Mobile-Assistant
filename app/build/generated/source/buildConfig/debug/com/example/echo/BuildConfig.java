79d240cf82bb67c9053a94385ce17dee9ca7c0ae
Muhammad Atta Ur-Rehman <chatta.ca67@gmail.com>
2025-07-09 18:38:11 +0500

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/example/echo/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.echo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.echo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String CLOUDINARY_API_KEY = "482883311769531";
+  // Field from build type: debug
+  public static final String CLOUDINARY_API_SECRET = "3wnDOT1sGqBlA1jrptGkIymooHQ";
+  // Field from build type: debug
+  public static final String CLOUDINARY_CLOUD_NAME = "djk93al88";
+  // Field from build type: debug
+  public static final String CLOUDINARY_UPLOAD_PRESET = "SWFM_unsigned";
+}
